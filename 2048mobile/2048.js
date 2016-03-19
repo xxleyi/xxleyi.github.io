5dc2ee50f58128c51bc612d4ca00616a2f6c0cf1
simon <qlnd303@163.com>
2016-03-19 20:16:06 +0800

    try for mobile.

---
+++ 2048mobile/2048.js
@@ -0,0 +1,295 @@
+var board = new Array();
+var moved = false;
+var lost_game = false;
+var score = 0;
+var direction = -1;
+var num_2048 = 0;
+var randomX = 0;
+var randomY= 0;
+$(document).ready(function(){
+  if(!(isPC())){
+    $("html").css("font-size","30px");
+  }
+  newgame();
+});
+
+function newgame(){
+  init();
+  generateOneNumber();
+  generateOneNumber();
+  updateBoardView();
+}
+
+function again(){
+  newgame();
+  $(".dialog-fail").css("display","none");
+}
+
+function conti() {
+  $(".dialog-success").css("display","none");
+}
+
+function init(){
+  score = 0;
+  num_2048 = 0;
+  $(".dialog-fail").css("display","none")
+  $(".dialog-success").css("display","none")
+  for(var i=0;i<4;i++){
+    for(var j=0;j<4;j++){
+      var gridCell = $("#grid-cell-"+i+"-"+j);
+    }
+  }
+  for(var i=0;i<4;i++){
+    board[i] = new Array();
+    for(var j=0;j<4;j++){
+      board[i][j] = 0;
+    }
+  }
+  var scoreCell = $("#score");
+  scoreCell.text("Score:000"+score);
+  updateBoardView();
+}
+
+function updateBoardView(){
+  $(".grid-cell").remove();
+  var scoreCell = $("#score");
+  scoreCell.text("Score:"+score);
+  for(var i=0;i<4;i++){
+    for(var j=0;j<4;j++){
+      $("#grid-container").append('<div class="grid-cell" id="grid-cell-'+i+'-'+j+'"></div>');
+      var theNumberCell = $('#grid-cell-'+i+"-"+j);
+      if(board[i][j]==0){
+        theNumberCell.text(' ');
+      }else {
+        theNumberCell.css("background-color", getNumberCellBgColor(board[i][j]));
+        theNumberCell.css("color", getNumberCellFontColor(board[i][j]));
+        theNumberCell.css("font-size", getNumberCellFontSize(board[i][j]));
+        theNumberCell.text(board[i][j]);
+      }
+    }
+  }
+  if(moved){
+    showNumberWithAnimation(randomX, randomY);
+  }
+}
+
+function generateOneNumber(){
+  var temp = new Array();
+  temp = getValidCell();
+  tempNum = temp[Math.floor(temp.length * Math.random())];
+  randomX = parseInt(tempNum / 4);
+  randomY = parseInt(tempNum % 4);
+  var randomNumber = Math.random()<0.7?2:4;
+  board[randomX][randomY] = randomNumber;
+}
+
+function showNumberWithAnimation(randomX, randomY) {
+  var theNumberCell = $("#grid-cell-"+randomX+"-"+randomY);
+  theNumberCell.animate({
+    opacity:'0.4',
+    width:"1.5rem",
+    marginLeft:"0rem",
+    marginRight:"4.5rem"
+  },100);
+  theNumberCell.animate({
+    opacity:'1',
+    width:"5rem",
+    marginLeft:"0.5rem",
+    marginRight:"0.5rem"
+  },200);
+}
+
+function stopAnimation(randomx, randomy) {
+  var theNumberCell = $("#grid-cell-"+randomX+"-"+randomY);
+  theNumberCell.stop();
+}
+
+$(document).keydown(function (event) {
+  stopAnimation(randomX, randomY);
+  move();
+  find2048();
+  if(moved){
+    generateOneNumber();
+  }else{
+    getValidCell();
+    if(lost_game){
+      findMerge();
+      if(lost_game){
+      $(".dialog-fail").css("display","block")
+      }
+    }
+  }
+  updateBoardView();
+  moved = false;
+
+})
+
+document.addEventListener('touchstart',function(event){
+	startx = event.touches[0].pageX;
+	starty = event.touches[0].pageY;
+});
+
+document.addEventListener('touchmove',function(event){
+	event.preventDefault();
+});
+
+document.addEventListener('touchend',function(event){
+	endx = event.changedTouches[0].pageX;
+	endy = event.changedTouches[0].pageY;
+
+	var deltax = endx - startx;
+	var deltay = endy - starty;
+
+	if(Math.abs(deltax) <0.03*documentWidth&& Math.abs(deltay)<0.01*documentWidth){
+		return;
+	}
+
+	if(Math.abs(deltax) >= Math.abs(deltay)){
+		if(deltax<0){
+      //left
+      direction = 0;
+		}else{
+      //right
+      direction = 1;
+    }
+  }else{
+    if(deltay<0){
+      //up
+      direction = 2;
+    }else{
+      //down
+      direction = 3;
+    }
+  }
+  stopAnimation();
+  moveMobile();
+  find2048();
+  if(moved){
+    generateOneNumber();
+  }else{
+    getValidCell();
+    if(lost_game){
+      findMerge();
+      if(lost_game){
+      $(".dialog-fail").css("display","block")
+      }
+    }
+  }
+  updateBoardView();
+  moved = false;
+});
+
+function move(){
+  rotate();
+  remove();
+  for(var i=0;i<4;i++){
+    for(var j=0;j<3;j++){
+        if(board[i][j]==board[i][j+1]){
+          board[i][j] *= 2;
+          score += board[i][j];
+          board[i][j+1] = 0;
+        }
+    }
+  }
+  remove();
+  if(event.keyCode==40){
+    for(var i=0;i<4;i++){
+      board[i].reverse();
+    }
+    transpose();
+  }else{
+    rotate();
+  }
+}
+
+function moveMobile(){
+  rotateMobile();
+  remove();
+  for(var i=0;i<4;i++){
+    for(var j=0;j<3;j++){
+        if(board[i][j]==board[i][j+1]){
+          board[i][j] *= 2;
+          score += board[i][j];
+          board[i][j+1] = 0;
+        }
+    }
+  }
+  remove();
+  if(direction==3){
+    for(var i=0;i<4;i++){
+      board[i].reverse();
+    }
+    transpose();
+  }else{
+    rotateMobile();
+  }
+}
+
+function remove() {
+  for(var i=0;i<4;i++){
+    for(var j=0;j<4;j++){
+      for(var k=0;k<3-j;k++){
+        if(board[i][k]==0){
+          board[i][k] = board[i][k+1];
+          board[i][k+1] = 0;
+          if(board[i][k]>0){
+            moved = true;
+          }
+        }
+      }
+    }
+  }
+}
+
+
+function rotate(){
+  switch (event.keyCode) {
+    case 37://left
+    event.preventDefault();
+      break;
+    case 38://up
+    event.preventDefault();
+      transpose();
+      break;
+    case 39://right
+    event.preventDefault();
+      for(var i=0;i<4;i++){
+        board[i].reverse();
+      }
+      break;
+    case 40://down
+    event.preventDefault();
+      transpose();
+      for(var i=0;i<4;i++){
+        board[i].reverse();
+      }
+    break;
+
+  }
+}
+
+function rotateMobile(){
+  switch (direction) {
+    case 0://left
+    event.preventDefault();
+      break;
+    case 2://up
+    event.preventDefault();
+      transpose();
+      break;
+    case 1://right
+    event.preventDefault();
+      for(var i=0;i<4;i++){
+        board[i].reverse();
+      }
+      break;
+    case 3://down
+    event.preventDefault();
+      transpose();
+      for(var i=0;i<4;i++){
+        board[i].reverse();
+      }
+    break;
+
+  }
+}
---
+++ 2048mobile/support2048.js
@@ -0,0 +1,167 @@
+/*
+name:support2048
+原作者:cqc
+改编：刘西民
+*/
+documentWidth = window.screen.width;
+documentHeight = window.screen.height;
+
+function getNumberCellBgColor(number){
+
+	switch(number){
+		case 2:return "#eee4da"; break;
+		case 4:return "#ede0c8"; break;
+		case 8:return "#f2b179"; break;
+		case 16:return "#f59563"; break;
+		case 32:return "#f67c5f"; break;
+		case 64:return "#ec6544"; break;
+		case 128:return "#e44d29"; break;
+		case 256:return "#edcf72"; break;
+		case 512:return "#c8a145"; break;
+		case 1024:return "#a8832b"; break;
+		case 2048:return "#86aa9c"; break;
+		case 4096:return "#a6c"; break;
+		case 8192:return "#791e6f"; break;
+	}
+	return "grey";
+
+}
+
+function isPC() {
+    var system = {
+        win : false,
+        mac : false,
+        xll : false
+    };
+    //检测平台
+    var p = navigator.platform;
+    system.win = p.indexOf("Win") == 0;
+    system.mac = p.indexOf("Mac") == 0;
+    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+    //跳转语句
+    if (system.win || system.mac || system.xll) { //转向电脑端
+        return true; //是电脑
+    } else {
+        return false; //是手机
+    }
+}
+
+function isMobile() {
+	var regex_match = /(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i;
+	var u = navigator.userAgent;
+	if (null == u) {
+		return true;
+	}
+	var result = regex_match.exec(u);
+	if (null == result) {
+		return false
+	} else {
+		return true
+	}
+}
+
+function getNumberCellFontSize(number){
+	if(number <= 64){
+		return "3em";
+	}else if(number <= 512){
+		return "2.5em";
+	}else if(number <=99999){
+		return "2em";
+	}else {
+		return "1.5em";
+	}
+return "1em";
+}
+
+function getNumberCellFontColor(number){
+	if(number <= 4){
+		return "#776e65";
+	}
+	return "white";
+}
+
+function getValidCell() {
+	var valid_list = new Array();
+	for(var i=0;i<4;i++){
+		for(var j=0;j<4;j++){
+			if(board[i][j]==0){
+				valid_list.push(4*i + j);
+			}
+		}
+	}
+	if(valid_list.length==0){
+		lost_game = true;
+	}
+	return valid_list;
+}
+
+function transpose(){
+  var temp_transpose = new Array();
+	for(var i=0;i<4;i++){
+		temp_transpose[i] = new Array();
+	    for(var j=0;j<4;j++){
+				temp_transpose[i][j] = board[j][i];
+	      }
+	}
+	for(var i=0;i<4;i++){
+	    for(var j=0;j<4;j++){
+				board[i][j] = temp_transpose[i][j];
+	      }
+	}
+}
+
+function findMerge() {
+	for(var i=0;i<4;i++){
+		for(var j=0;j<3;j++){
+			if(board[i][j]==board[i][j+1]){
+				lost_game = false;
+			}
+		}
+	}
+	for(var i=0;i<3;i++){
+		for(var j=0;j<4;j++){
+			if(board[i][j]==board[i+1][j]){
+				lost_game = false;
+			}
+		}
+	}
+}
+
+function find2048() {
+	for(var i=0;i<4;i++){
+		for(var j=0;j<4;j++){
+			if(board[i][j]>0){
+				if(num_2048==0){
+					if(board[i][j]%2048==0){
+						num_2048 += 1;
+						$(".dialog-success").css("display","block");
+						$("#conti").text("Good! Try "+4096);
+					}
+				}else if(num_2048==1){
+					if(board[i][j]%4096==0){
+						num_2048 += 1;
+						$(".dialog-success").css("display","block");
+						$("#conti").text("Better! Try "+8192);
+					}else if(num_2048==2){
+						if(board[i][j]%8192==0){
+							num_2048 += 1;
+							$(".dialog-success").css("display","block");
+							$("#conti").text("You are the Best, Try "+16384)
+						}else if (num_2048==3){
+							if(board[i][j]%16384==0){
+								num_2048 += 1;
+								$(".dialog-success").css("display","block");
+								$("#conti").text("You Win!")
+							}
+						}
+					}
+				}
+				// else{
+				// 	return false;
+				// }
+			}
+
+		}
+	}
+	console.log("num_2048:"+num_2048)
+}
