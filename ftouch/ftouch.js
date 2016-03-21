dc90af6dff5f121b695364a2c2d3347bd253ecf6
simon <qlnd303@163.com>
2016-03-21 22:12:21 +0800

    try for phone

--- ftouch/ftouch.js
+++ ftouch/ftouch.js
@@ -3,6 +3,15 @@
 $(document).ready(function() {
   init();
   // newgame();
+  // var mButton = document.getElementById("middle-button");
+  // $(mButton).css("background-color","grey");
+  var mButton = document.getElementById("middle-button");
+  // $(mButton).css("background-color","grey");
+  console.log("ready")
+  mButton.addEventListener("touchend",function(event){
+    $("#score-button").css("background-color","grey");
+    console.log("shit")
+  })
 
 })
 
@@ -13,8 +22,3 @@ function init() {
 // function newgame() {
 //
 // }
-
-document.addEventListener("touchend",function(){
-  $("#score-button").css("background-color","grey");
-  console.log("shit")
-})
