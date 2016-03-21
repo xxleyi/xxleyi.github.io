76ea5b5ed16d773bf6a7c2f2e4b1295e655702aa
simon <qlnd303@163.com>
2016-03-21 22:30:09 +0800

    try for phone

--- ftouch/ftouch.js
+++ ftouch/ftouch.js
@@ -1,23 +1,20 @@
-
-
+var score = 0;
 $(document).ready(function() {
   init();
-  // newgame();
-  // var mButton = document.getElementById("middle-button");
-  // $(mButton).css("background-color","grey");
   var mButton = document.getElementById("middle-button");
-  // $(mButton).css("background-color","grey");
-  console.log("ready")
-  mButton.addEventListener("touchend",function(event){
-    $("#score-button").css("background-color","grey");
-    console.log("shit")
-  })
 
+  mButton.addEventListener("touchstart",function(event){
+  $("#score-button").css("background-color","grey");
+  })
+  mButton.addEventListener("touchend",function(event){
+  $("#score-button").css("background-color","red");
+  })
 })
 
 function init() {
-  $("#score-button").text("score:"+100);
+  $("#score-button").text("score:"+0);
 }
+
 
 // function newgame() {
 //
